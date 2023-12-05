f3558e727b6a78e34984e46701d821e0462148d0
= <mehmetdjigo@gmail.com>
2023-12-05 21:00:36 +0000

    Ajout de la  liste des matieres

--- app/Http/Controllers/MatiereController.php
+++ app/Http/Controllers/MatiereController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Matiere;
 use Illuminate\Http\Request;
 
 class MatiereController extends Controller
 {
-    //
+    public function home(){
+        $listeMatieres = Matiere::all();
+        return view("matieres.liste",["matieres"=>$listeMatieres]);
+    }
 }
